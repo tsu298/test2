235fc3435df3351f52bab98cb4a1dd37d268369c
tabuchi ryoya <rnhxq574_0827@yahoo.co.jp>
2024-11-05 23:55:52 +0900

    リモートリポジトリの変更

---
+++ src/php03/config/cities.php
@@ -0,0 +1,39 @@
+<?php
+
+$cities = [
+    [
+        'name' => 'シドニー',
+        'time_zone' => 'Australia/Sydney',
+        'national_flag_img' => 'australia.jpg'
+    ],
+    [
+        'name' => '東京',
+        'time_zone' => 'Asia/Tokyo',
+        'national_flag_img' => 'japan.jpg'
+    ],
+    [
+        'name' => '上海',
+        'time_zone' => 'Asia/Shanghai',
+        'national_flag_img' => 'china.jpg'
+    ],
+    [
+        'name' => 'モスクワ',
+        'time_zone' => 'Europe/Moscow',
+        'national_flag_img' => 'russia.jpg'
+    ],
+    [
+        'name' => 'ロンドン',
+        'time_zone' => 'Europe/London',
+        'national_flag_img' => 'british.jpg'
+    ],
+    [
+        'name' => 'ヨハネスブルグ',
+        'time_zone' => 'Africa/Johannesburg',
+        'national_flag_img' => 'south-africa.jpg'
+    ],
+    [
+        'name' => 'ニューヨーク',
+        'time_zone' => 'America/New_York',
+        'national_flag_img' => 'america.jpg'
+    ],
+    ];
